466fc867176b327543daae09e8b4fdd39ac8d784
Nigraum <yan.corintima@hotmail.com>
2022-10-11 09:50:30 -0300

    Create container style on styles.js of header

--- src/components/Header/Styles.js
+++ src/components/Header/Styles.js
@@ -1,2 +1,18 @@
 import styled from "styled-components";
 import { colors } from "../../styles/global";
+import { device } from "../../utils/constants/sizes";
+
+export const Container = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+
+position: absolute;
+top: 0;
+height: 6.3rem;
+width: 100%;
+
+background-color: rgba(59, 32, 11, 0.05);
+color: ${colors.brown};
+backdrop-filter: blur(10px);
+`;
