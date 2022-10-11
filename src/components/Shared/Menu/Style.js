39d0fa09cf5533e2dff25f73fa20399a3002e411
Nigraum <yan.corintima@hotmail.com>
2022-10-11 10:43:14 -0300

    Create container const for menu.jsx

---
+++ src/components/Shared/Menu/Style.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { colors } from "../../../styles/global";
+import { device } from "../../../utils/constants/sizes";
+
+export const Container = styled.div`
+display: none;
+z-index: 99;
+
+input {
+  display: none;
+}
+
+input:checked ~ label #hamburguer {
+  transform: rotate(45deg);
+}
+
+input:checked ~ label #hamburguer:before {
+  transform: rotate(90deg);
+  top: 0;
+}
+
+input:checked ~ label #hamburguer:after {
+  transform: rotate(90deg);
+  bottom: 0;
+}
+
+@media screen and (${device.ipad}) {
+  display: block;
+}
+`;
