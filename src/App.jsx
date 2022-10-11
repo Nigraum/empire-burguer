32bbdb23788d87a2ddd4969cfb343f006c17f527
Nigraum <yan.corintima@hotmail.com>
2022-10-11 11:33:55 -0300

    Add home component to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { GlobalStyle } from "./styles/global";
-import Header from "./components/Header/Header"
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div>
       <GlobalStyle />
-      <Header />
+      <Home />
     </div>
   );
 };
