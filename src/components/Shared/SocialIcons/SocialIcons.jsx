7f67dd5d50d3164a5d622fa6e5f101c6c542379c
Nigraum <yan.corintima@hotmail.com>
2022-10-11 10:39:02 -0300

    Finishing socialIcons.jsx

--- src/components/Shared/SocialIcons/SocialIcons.jsx
+++ src/components/Shared/SocialIcons/SocialIcons.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Constainer } from "./Style";
 import { BsInstagram } from "react-icons/bs";
 import Ifood from "../SVG/Ifood;"
 
 const SocialIcons = ({ color }) => {
   return (
-    
-  )
-}
+    <Constainer>
+      <Ifood color={color} style={{ width: "3rem", marginRight: "1.8rem" }} />
+      <BsInstagram size={"2.25rem"} color={color} />
+    </Constainer>
+  );
+};
+
+export default SocialIcons;
