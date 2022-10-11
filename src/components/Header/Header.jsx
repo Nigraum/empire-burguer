835a30b3b78a1f30651a81a5140008f5755030f9
Nigraum <yan.corintima@hotmail.com>
2022-10-11 11:26:30 -0300

    Finishing header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,17 +9,49 @@ import {
   VerticalLine
  } from "./Styles";
 
- import SocialIcons from "../Shared/SocialIcons/SocialIcons";
- import Logo from "../Shared/SVG/Logo";
+import SocialIcons from "../Shared/SocialIcons/SocialIcons";
+import Logo from "../Shared/SVG/Logo";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Header = () => {
   return (
     <Container>
       <Navigation>
         <LogoContainer>
-          
+          <Logo
+            color={colors.brown}
+            style={{ marginRight: "0.75rem", height: "3rem" }}
+          />
+          <h1>
+            <span>Empire </span>burguer
+          </h1>
         </LogoContainer>
+
+        <Nav>
+          <li>Home</li>
+          <li>Promoção</li>
+          <li>Cardápio</li>
+          <li>Comentários</li>
+          <li>Contato</li>
+        </Nav>
+
+        <SocialMedia>
+          <SocialIcons color="#FFF" />
+          <VerticalLine />
+          <button>
+            <BsWhatsapp
+            size={"2.25rem"}
+            style={{
+              marginRight: ".9rem",
+              padding: 0,
+              }}
+            />
+            <span>Contato</span>
+          </button>
+        </SocialMedia>
       </Navigation>
     </Container>
-  )
-}
+  );
+};
+
+export default Header;
