6ff36d7eb7ad171351ef7a7ee82d4f2914d602ee
Nigraum <yan.corintima@hotmail.com>
2022-10-11 10:23:30 -0300

    Importing components from style.js to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import { colors } from "../../styles/global";
+import { 
+  Container,
+  Navigation,
+  LogoContainer,
+  Nav,
+  SocialMedia,
+  VerticalLine
+ } from "./Styles";
 
 const Header = () => {
   return (
