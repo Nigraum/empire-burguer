752cb58d67eaf142d0b769edd1ccfd4c9b45ac0c
Nigraum <yan.corintima@hotmail.com>
2022-10-11 10:45:08 -0300

    Testing container component on menu

---
+++ src/components/Shared/Menu/Menu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "./Style";
+
+const Menu = () => {
+  return (
+    <Container>
+      <input id="menu-hamburguer" type="checkbox" />
+      
+    </Container>
+  );
+};
