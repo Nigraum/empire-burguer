24618574eb79baa176c829737e60fb09faae99e8
Nigraum <yan.corintima@hotmail.com>
2022-10-12 09:54:56 -0300

    Add responsiveness to style.js of banner.jsx

--- src/components/Banner/Style.js
+++ src/components/Banner/Style.js
@@ -3,12 +3,12 @@ import { colors } from "../../styles/global";
 import { device } from "../../utils/constants/sizes";
 
 export const Container = styled.section`
-display: flex;
-justify-content: center;
-align-items: center;
-position: relative;
-width: 100vw;
-margin-bottom: 13rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  width: 100vw;
+  margin-bottom: 13rem;
 
 img {
   width: 100%;
@@ -16,5 +16,13 @@ img {
   object-fit: cover;
   top: 0;
   z-index: -1;
+}
+
+@media screen and (${device.ipad}) {
+  margin-bottom: 24rem;
+
+  img {
+    object-position: 57% 50%;
+  }
 }
 `;
