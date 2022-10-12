47680ffacd8d895363ca9a8ab8b8ec86c18e647a
Nigraum <yan.corintima@hotmail.com>
2022-10-12 09:43:56 -0300

    Setting banner on home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container } from "./Style";
 import Header from "../Header/Header";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
     <Container>
       <Header />
+      <Banner />
     </Container>
   );
 };
