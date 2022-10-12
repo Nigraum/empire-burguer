bf2eaad97991a6c1975ad120a4f306a8e842da9b
Nigraum <yan.corintima@hotmail.com>
2022-10-12 09:53:10 -0300

    Testing images on container

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Container } from "./Style";
 
+import banner from "../../assets/images/banner.png";
+import banner_mobile from "../../assets/images/banner_mobile.png";
+
 const Banner = () => {
   return (
     <Container>
-      
+      <img src={banner} />
+      <img src={banner_mobile} />
     </Container>
   );
 };
--- src/components/Banner/Style.js
+++ src/components/Banner/Style.js
@@ -3,5 +3,18 @@ import { colors } from "../../styles/global";
 import { device } from "../../utils/constants/sizes";
 
 export const Container = styled.section`
-background: #fff;
+display: flex;
+justify-content: center;
+align-items: center;
+position: relative;
+width: 100vw;
+margin-bottom: 13rem;
+
+img {
+  width: 100%;
+  height: 545px;
+  object-fit: cover;
+  top: 0;
+  z-index: -1;
+}
 `;
