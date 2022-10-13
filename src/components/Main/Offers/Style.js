c99bcb7b10d70485a7b661c0ce3f12461d36e404
Nigraum <yan.corintima@hotmail.com>
2022-10-13 12:16:20 -0300

    Formatting text of offers.jsx

--- src/components/Main/Offers/Style.js
+++ src/components/Main/Offers/Style.js
@@ -3,5 +3,24 @@ import { colors } from "../../../styles/global";
 import { device } from "../../../utils/constants/sizes";
 
 export const Container = styled.section`
-  background-color: #fff;
+  h2 {
+    font-family: "Lilita One";
+    font-size: 3.2rem;
+    font-weight: 400;
+    color: ${colors.brown};
+    text-transform: uppercase;
+  }
+
+  p {
+    width: 100%;
+    font-size: 1.6rem;
+    font-weight: 400;
+    margin-bottom: 1.6rem;
+    color: ${colors.brown_secondary};
+
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+  }
 `;
