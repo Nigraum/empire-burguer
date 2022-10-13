1e00f0fe63048e712dc7d23afc6051517226a6ef
Nigraum <yan.corintima@hotmail.com>
2022-10-13 13:03:10 -0300

    Starting to make skeleton of closingtime and style it

--- src/components/Main/ClosingTime/ClosingTime.jsx
+++ src/components/Main/ClosingTime/ClosingTime.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Container } from "./Style";
+import clockicon from "../../../assets/images/clockicon.png";
+import { colors } from "../../../styles/global";
+import { days_open } from "../../../utils/constants/days_open";
 
 const ClosingTime = () => {
   return(
     <Container>
-      <h1>Test</h1>
+      <img src={clockicon} alt="icon" />
+      <div>
+          <h3>Horário de funcionamento</h3>
+          <h4>Segunda a sexta: 17h00 - 23h00</h4>
+          <h4>Sabado a domingo: 18h00 - 23h00</h4>
+      </div>
     </Container>
   );
 };
