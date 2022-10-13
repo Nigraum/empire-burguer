3b8fa6b6c9f284a8a198c6d3c2f943b93e0467c5
Nigraum <yan.corintima@hotmail.com>
2022-10-13 13:35:52 -0300

    Create function for closed or open

--- src/components/Main/ClosingTime/ClosingTime.jsx
+++ src/components/Main/ClosingTime/ClosingTime.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Badge, Container, Hashtag, VerticalLine } from "./Style";
 import clockicon from "../../../assets/images/clockicon.png";
 import { colors } from "../../../styles/global";
 import { days_open } from "../../../utils/constants/days_open";
 
 const ClosingTime = () => {
+  const [isClosed, setIsClosed] = useState(true);
+  const milliseconds = 2000;
+
+  setInterval (() => {
+    const currentDate = new Date();
+    const currentHour = currentDate.getHours();
+    const currentDay = currentDate.getDay();
+    let initialTime = 17;
+    let finalTime = 23;
+
+    if (
+      currentDay >= days_open["segunda"] &&
+      currentDay <= days_open["sexta"]
+    ) {
+      initialTime = 17;
+    } else {
+      initialTime = 18;
+    }
+
+    if (currentHour >= initialTime && currentHour < finalTime) {
+      setIsClosed(false);
+    } else {
+      setIsClosed(true);
+    }
+  }, milliseconds);
+  
   return(
     <Container>
       <Badge>
