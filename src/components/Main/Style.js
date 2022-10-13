dce38e9f50a309581609b5d7efdb805edbd76bdc
Nigraum <yan.corintima@hotmail.com>
2022-10-13 12:42:53 -0300

    Formatting style of main.jsx

--- src/components/Main/Style.js
+++ src/components/Main/Style.js
@@ -2,6 +2,10 @@ import styled from "styled-components";
 import { device } from "../../utils/constants/sizes";
 
 export const Container = styled.div`
-background: #fff;
-width: 200rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 117rem;
+  margin: 0 auto;
+  margin-bottom: 10rem;
 `;
