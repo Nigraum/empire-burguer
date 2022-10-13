c5e1562869dc33f994f0250bd5b9060c897c225b
Nigraum <yan.corintima@hotmail.com>
2022-10-13 12:05:40 -0300

    Create main component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <Container>
+      
+    </Container>
+  );
+};
+
+export default Main;
---
+++ src/components/Main/Style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { device } from "../../utils/constants/sizes";
+
+export const Container = styled.div`
+background: #fff;
+width: 200rem;
+`;
