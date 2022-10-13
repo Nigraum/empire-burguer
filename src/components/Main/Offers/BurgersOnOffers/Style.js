f1098ea080644db56f670716b4bb0af41e0aef07
Nigraum <yan.corintima@hotmail.com>
2022-10-13 12:36:21 -0300

    Finishing styling container of burguers on offer

--- src/components/Main/Offers/BurgersOnOffers/Style.js
+++ src/components/Main/Offers/BurgersOnOffers/Style.js
@@ -9,6 +9,39 @@ export const Container = styled.div`
   grid-gap: 3rem;
   row-gap: 2.6rem;
   margin: 0 auto;
+
+  img {
+    object-fit: cover;
+  }
+
+  article:nth-child(1) {
+    grid-row: 1/3;
+  }
+
+  article {
+    position: relative;
+
+    span {
+      position: absolute;
+      bottom: 2rem;
+      right: 2rem;
+
+      h4 {
+        font-size: 1.9rem;
+        color: ${colors.brown};
+        font-weight: 100;
+        margin-bottom: 1rem;
+      }
+
+      h3 {
+        color: ${colors.white};
+        font-family: "Lilita One";
+        font-size: 3.7rem;
+        text-transform: uppercase;
+        line-height: 2.2rem;
+      }
+    }
+  }
 `;
 
 export const OfferInfo = styled.div`
