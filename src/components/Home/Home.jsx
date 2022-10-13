f07bcb8417b06296b81228cdde56f15c7d99b365
Nigraum <yan.corintima@hotmail.com>
2022-10-13 12:06:09 -0300

    Testing main on home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Container } from "./Style";
 import Header from "../Header/Header";
 import Banner from "../Banner/Banner";
+import Main from "../Main/Main";
 
 const Home = () => {
   return (
     <Container>
       <Header />
       <Banner />
+      <Main />
     </Container>
   );
 };
