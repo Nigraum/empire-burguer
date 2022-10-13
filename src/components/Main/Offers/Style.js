59a613092ec47bd4029f83a85c2f15b70d72ad34
Nigraum <yan.corintima@hotmail.com>
2022-10-13 12:53:22 -0300

    Add responsiveness to offers.jsx

--- src/components/Main/Offers/Style.js
+++ src/components/Main/Offers/Style.js
@@ -3,6 +3,9 @@ import { colors } from "../../../styles/global";
 import { device } from "../../../utils/constants/sizes";
 
 export const Container = styled.section`
+  margin: 0 auto;
+  margin-bottom: 6.4rem;
+  
   h2 {
     font-family: "Lilita One";
     font-size: 3.2rem;
@@ -22,5 +25,25 @@ export const Container = styled.section`
     -webkit-line-clamp: 2;
     -webkit-box-orient: vertical;
     overflow: hidden;
+  }
+
+  @media screen and (${device.laptop}) {
+    p {
+      width: 70.5rem;
+    }
+  }
+
+  @media screen and (${device.ipad}) {
+    p {
+      width: 40rem;
+    }
+  }
+
+  @media screen and (${device.ipad}) {
+    margin-bottom: 3.79rem;
+
+    p {
+      width: 37rem;
+    }
   }
 `;
