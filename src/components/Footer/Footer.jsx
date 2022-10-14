0f7dd6509f892dc15657536ce8ec11ea112e595f
Nigraum <yan.corintima@hotmail.com>
2022-10-14 13:42:07 -0300

    Importing googlemaps for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,11 +3,14 @@ import { colors } from "../../styles/global";
 import { sizes } from "../../utils/constants/sizes";
 import { Container } from "./Style";
 
+import GoogleMaps from "../Shared/GoogleMaps/GoogleMaps"
+
 const Footer = () => {
   return (
     <Container >
       <h2>Onde fica nosso castelo</h2>
       <h3>Estaremos de porta aberta para a vossa realeza</h3>
+      <GoogleMaps />
     </Container>
   );
 };
