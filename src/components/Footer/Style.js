3cfef10e92ace1e4d688644323d35c3bbeda45e6
Nigraum <yan.corintima@hotmail.com>
2022-10-14 13:39:27 -0300

    Styling h2 of container

--- src/components/Footer/Style.js
+++ src/components/Footer/Style.js
@@ -3,6 +3,19 @@ import { colors } from "../../styles/global";
 import { device } from "../../utils/constants/sizes";
 
 export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
 
-width: 100vw;
+  h2 {
+    color: ${colors.brown};
+    text-transform: uppercase;
+    font-size: 3.2rem;
+    font-family: "Lilita One";
+    margin-bottom: 0.4rem;
+    line-height: 3.5rem;
+  }
+
+  
 `;
